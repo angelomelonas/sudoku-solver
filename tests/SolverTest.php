1ce17b3f0f45f2230828dc562cdefadc8eb41bdb
Angelo Melonas <angelomelonas@gmail.com>
2020-02-02 00:40:08 +0100

    Improved tests.

--- tests/SolverTest.php
+++ tests/SolverTest.php
@@ -3,6 +3,7 @@
 namespace sudoku\solver\test;
 
 use PHPUnit\Framework\TestCase;
+use sudoku\solver\common\object\Puzzle;
 use sudoku\solver\solver\Solver;
 
 /**
@@ -12,69 +13,78 @@ use sudoku\solver\solver\Solver;
 class SolverTest extends TestCase
 {
     /**
-     * @test
      */
-    public function basicSingleEmptyCellSolverTest()
+    public function testBasicSingleEmptyCellSolver()
     {
-        $puzzleInput = array(
-            array(0, 3, 9, 1, 4, 8, 2, 5, 7),
-            array(1, 5, 8, 2, 7, 9, 4, 6, 3),
-            array(2, 7, 4, 5, 6, 3, 8, 9, 1),
-            array(7, 1, 6, 4, 2, 5, 3, 8, 9),
-            array(3, 4, 5, 9, 8, 7, 1, 2, 6),
-            array(9, 8, 2, 6, 3, 1, 5, 7, 4),
-            array(8, 6, 1, 3, 9, 2, 7, 4, 5),
-            array(5, 9, 7, 8, 1, 4, 6, 3, 2),
-            array(4, 2, 3, 7, 5, 6, 9, 1, 8));
+        $puzzleInput = [
+            [0, 3, 9, 1, 4, 8, 2, 5, 7],
+            [1, 5, 8, 2, 7, 9, 4, 6, 3],
+            [2, 7, 4, 5, 6, 3, 8, 9, 1],
+            [7, 1, 6, 4, 2, 5, 3, 8, 9],
+            [3, 4, 5, 9, 8, 7, 1, 2, 6],
+            [9, 8, 2, 6, 3, 1, 5, 7, 4],
+            [8, 6, 1, 3, 9, 2, 7, 4, 5],
+            [5, 9, 7, 8, 1, 4, 6, 3, 2],
+            [4, 2, 3, 7, 5, 6, 9, 1, 8]
+        ];
 
-        $puzzleExpectedOutput = array(
-            array(6, 3, 9, 1, 4, 8, 2, 5, 7),
-            array(1, 5, 8, 2, 7, 9, 4, 6, 3),
-            array(2, 7, 4, 5, 6, 3, 8, 9, 1),
-            array(7, 1, 6, 4, 2, 5, 3, 8, 9),
-            array(3, 4, 5, 9, 8, 7, 1, 2, 6),
-            array(9, 8, 2, 6, 3, 1, 5, 7, 4),
-            array(8, 6, 1, 3, 9, 2, 7, 4, 5),
-            array(5, 9, 7, 8, 1, 4, 6, 3, 2),
-            array(4, 2, 3, 7, 5, 6, 9, 1, 8));
+        $puzzleExpectedOutput = [
+            [6, 3, 9, 1, 4, 8, 2, 5, 7],
+            [1, 5, 8, 2, 7, 9, 4, 6, 3],
+            [2, 7, 4, 5, 6, 3, 8, 9, 1],
+            [7, 1, 6, 4, 2, 5, 3, 8, 9],
+            [3, 4, 5, 9, 8, 7, 1, 2, 6],
+            [9, 8, 2, 6, 3, 1, 5, 7, 4],
+            [8, 6, 1, 3, 9, 2, 7, 4, 5],
+            [5, 9, 7, 8, 1, 4, 6, 3, 2],
+            [4, 2, 3, 7, 5, 6, 9, 1, 8]
+        ];
 
         $this->assertSolverOutput($puzzleInput, $puzzleExpectedOutput);
     }
 
     /**
-     * @test
      */
-    public function basicMultipleEmptyCellSolverTest()
+    public function testBasicMultipleEmptyCellSolver()
     {
-        $puzzleInput = array(
-            array(0, 3, 9, 1, 4, 0, 2, 5, 0),
-            array(1, 5, 8, 2, 7, 9, 0, 6, 3),
-            array(2, 7, 4, 0, 6, 3, 8, 9, 1),
-            array(0, 1, 6, 4, 0, 5, 3, 8, 0),
-            array(3, 4, 5, 0, 8, 0, 0, 2, 6),
-            array(9, 8, 2, 6, 0, 1, 5, 7, 4),
-            array(0, 6, 1, 3, 9, 2, 0, 4, 5),
-            array(5, 9, 0, 8, 1, 4, 6, 3, 2),
-            array(4, 2, 3, 7, 5, 6, 9, 0, 8));
+        $puzzleInput = [
+            [0, 3, 9, 1, 4, 0, 2, 5, 0],
+            [1, 5, 8, 2, 7, 9, 0, 6, 3],
+            [2, 7, 4, 0, 6, 3, 8, 9, 1],
+            [0, 1, 6, 4, 0, 5, 3, 8, 0],
+            [3, 4, 5, 0, 8, 0, 0, 2, 6],
+            [9, 8, 2, 6, 0, 1, 5, 7, 4],
+            [0, 6, 1, 3, 9, 2, 0, 4, 5],
+            [5, 9, 0, 8, 1, 4, 6, 3, 2],
+            [4, 2, 3, 7, 5, 6, 9, 0, 8]
+        ];
 
-        $puzzleExpectedOutput = array(
-            array(6, 3, 9, 1, 4, 8, 2, 5, 7),
-            array(1, 5, 8, 2, 7, 9, 4, 6, 3),
-            array(2, 7, 4, 5, 6, 3, 8, 9, 1),
-            array(7, 1, 6, 4, 2, 5, 3, 8, 9),
-            array(3, 4, 5, 9, 8, 7, 1, 2, 6),
-            array(9, 8, 2, 6, 3, 1, 5, 7, 4),
-            array(8, 6, 1, 3, 9, 2, 7, 4, 5),
-            array(5, 9, 7, 8, 1, 4, 6, 3, 2),
-            array(4, 2, 3, 7, 5, 6, 9, 1, 8));
+        $puzzleExpectedOutput = [
+            [6, 3, 9, 1, 4, 8, 2, 5, 7],
+            [1, 5, 8, 2, 7, 9, 4, 6, 3],
+            [2, 7, 4, 5, 6, 3, 8, 9, 1],
+            [7, 1, 6, 4, 2, 5, 3, 8, 9],
+            [3, 4, 5, 9, 8, 7, 1, 2, 6],
+            [9, 8, 2, 6, 3, 1, 5, 7, 4],
+            [8, 6, 1, 3, 9, 2, 7, 4, 5],
+            [5, 9, 7, 8, 1, 4, 6, 3, 2],
+            [4, 2, 3, 7, 5, 6, 9, 1, 8]
+        ];
 
         $this->assertSolverOutput($puzzleInput, $puzzleExpectedOutput);
     }
 
 
+    /**
+     * @param array $puzzleInput
+     * @param array $puzzleExpectedOutput
+     */
     private function assertSolverOutput(array $puzzleInput, array $puzzleExpectedOutput)
     {
-        $solver = new Solver($puzzleInput);
-        $this->assertEquals($puzzleExpectedOutput, $solver->solve());
+        $puzzle = new Puzzle($puzzleInput);
+        $solver = new Solver($puzzle);
+        $solvedPuzzle = $solver->solvePuzzle();
+
+        static::assertEquals($puzzleExpectedOutput, $solvedPuzzle->getPuzzleArray());
     }
 }
