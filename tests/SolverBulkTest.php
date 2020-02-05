21f0a1d97c5486fd95af34986a16f9a8a8f41463
Angelo Melonas <angelomelonas@gmail.com>
2020-02-05 22:11:08 +0100

    Added a bulk solver test.

---
+++ tests/SolverBulkTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace sudoku\solver\test;
+
+use PHPUnit\Framework\TestCase;
+use sudoku\solver\common\exception\SudokuSolverException;
+use sudoku\solver\common\exception\SudokuSolverExceptionFileSystem;
+use sudoku\solver\parser\PuzzleParser;
+use sudoku\solver\puzzle\code\Puzzle;
+use sudoku\solver\solver\Solver;
+
+/**
+ * @author Angelo Melonas <angelomelonas@gmail.com>
+ * @since 20200205 Initial creation.
+ */
+class SolverBulkTest extends TestCase
+{
+    /**
+     * Puzzle filepath constants.
+     */
+    const PATH_EASY_PUZZLES = '/puzzles/easy.csv';
+    const PATH_INTERMEDIATE_PUZZLES = '/puzzles/intermediate.csv';
+    const PATH_EXPERT_PUZZLES = '/puzzles/expert.csv';
+
+    /**
+     * Testing print constants.
+     */
+    const PUZZLES_SOLVED = 'Solving %s/%s puzzles...' . "\n";
+
+    /**
+     * @throws SudokuSolverException
+     * @throws SudokuSolverExceptionFileSystem
+     */
+    public function testAllPuzzleSimple()
+    {
+        $this->solveAllTests(PuzzleParser::parsePuzzleFromCsvFile(self::PATH_EASY_PUZZLES));
+    }
+
+    /**
+     * @throws SudokuSolverException
+     * @throws SudokuSolverExceptionFileSystem
+     */
+    public function testAllPuzzleEasy()
+    {
+        $this->solveAllTests(PuzzleParser::parsePuzzleFromCsvFile(self::PATH_EASY_PUZZLES));
+    }
+
+    /**
+     * @throws SudokuSolverException
+     * @throws SudokuSolverExceptionFileSystem
+     */
+    public function testAllPuzzleIntermediate()
+    {
+        $this->solveAllTests(PuzzleParser::parsePuzzleFromCsvFile(self::PATH_EASY_PUZZLES));
+    }
+
+    /**
+     * @throws SudokuSolverException
+     * @throws SudokuSolverExceptionFileSystem
+     */
+    public function testAllPuzzleExpert()
+    {
+        $this->solveAllTests(PuzzleParser::parsePuzzleFromCsvFile(self::PATH_EASY_PUZZLES));
+    }
+
+    /**
+     * @param array $allPuzzle
+     */
+    private function solveAllTests(array $allPuzzle)
+    {
+        $numberOfPuzzle = count($allPuzzle) / 2;
+
+        for ($puzzleIndex = 0; $puzzleIndex < count($allPuzzle); $puzzleIndex += 2) {
+            $this->assertSolverOutput(
+                $allPuzzle[$puzzleIndex],
+                $allPuzzle[$puzzleIndex + 1],
+                $numberOfPuzzle,
+                $puzzleIndex
+            );
+        }
+    }
+
+    /**
+     * @param Puzzle $puzzleInput
+     * @param Puzzle $puzzleExpectedOutput
+     * @param int $numberOfPuzzle
+     * @param int $numberPuzzle
+     */
+    private function assertSolverOutput(
+        Puzzle $puzzleInput,
+        Puzzle $puzzleExpectedOutput,
+        int $numberOfPuzzle,
+        int $numberPuzzle
+    ) {
+        $solver = new Solver($puzzleInput);
+        $solvedPuzzle = $solver->solvePuzzle();
+
+        print_r(vsprintf(self::PUZZLES_SOLVED, [($numberPuzzle / 2) + 1, $numberOfPuzzle]));
+        static::assertEquals($puzzleExpectedOutput->getPuzzleArray(), $solvedPuzzle->getPuzzleArray());
+    }
+}
